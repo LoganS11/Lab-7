a0d1a0795096e92bf7c1bb5737e79cf0e1b816af
Logan Schlachter <lschlac@rockets.utoledo.edu>
2020-11-16 16:50:16 -0500

    Lab 10

---
+++ Lab 7/src/Lab10JAVA/PaintEstimator.java
@@ -0,0 +1,32 @@
+package Lab10JAVA;
+import java.util.Scanner;
+import java.lang.Math;     // Note: Needed for math functions in part (3)
+
+public class PaintEstimator {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      double wallHeight;
+      double wallWidth;
+      double wallArea;
+      double paintGal;
+      int cans;
+      
+      
+      System.out.println("Enter wall height (feet):");
+      wallHeight = scnr.nextDouble();
+      
+      System.out.println("Enter wall width (feet):");
+      wallWidth = scnr.nextDouble();                      // FIXME (1): Prompt user to input wall's width
+      
+      // Calculate and output wall area
+      wallArea = (wallHeight * wallWidth);                        // FIXME (1): Calculate the wall's area
+      System.out.println("Wall area: " + wallArea + " square feet");     // FIXME (1): Finish the output statement
+      
+      // FIXME (2): Calculate and output the amount of paint in gallons needed to paint the wall
+      paintGal = (wallArea / 350);
+      System.out.println("Paint needed: " + paintGal + " gallons");
+      // FIXME (3): Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+      cans = (int) Math.round(paintGal + .5);
+      System.out.println("Cans needed: " + cans + " can(s)");
+   }
+}
